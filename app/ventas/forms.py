2cea6e1e832f568a9c486151475e120b4f867172
Javier Paredes <javier.paredes@unnaki.com.py>
2021-07-22 14:49:01 -0400

    Estructura de la vista para la venta

--- app/ventas/forms.py
+++ app/ventas/forms.py
@@ -13,8 +13,14 @@ class VentaForm(forms.ModelForm):
         }
         widgets = {
             'cliente':forms.Select(attrs={'class':'form-control' }),
-            'fecha_venta': forms.DateInput(format='%Y-%m-%d',attrs={'class':'form-control','value':datetime.now().strftime('%Y-%m-%d')}),
+            'fecha_venta': forms.DateInput(format='%Y-%m-%d',attrs={
+                'class':'form-control datetimepicker-input',
+                'id':'fecha_venta',
+                'data-target':'#fecha_venta',
+                'data-toggle':'datetimepicker',
+                'value':datetime.now().strftime('%Y-%m-%d')
+            }),
             'condicion_venta':forms.Select(attrs={'class':'form-control' }),
-			'monto_total' : forms.NumberInput(attrs={'class':'form-control'}),
-			'total_iva_10' : forms.NumberInput(attrs={'class':'form-control'}),
+			'monto_total' : forms.NumberInput(attrs={'class':'form-control','readonly':True}),
+			'total_iva_10' : forms.NumberInput(attrs={'class':'form-control','readonly':True}),
         }
--- app/ventas/views.py
+++ app/ventas/views.py
@@ -1,7 +1,12 @@
+from django.http.response import JsonResponse
 from .models import Venta
 from .forms import VentaForm
+from telas.models import Tela
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse_lazy
 
 
@@ -17,3 +22,24 @@ class VentaCreate(LoginRequiredMixin, generic.CreateView):
     template_name="ventas/venta_form.html"
     success_url=reverse_lazy("ventas:ventas_list")
     login_url="bases:login"
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'search_tela':
+                data = []
+                telas  = Tela.objects.filter(Q(codigo__icontains=request.POST['term']) | Q(nombre__icontains=request.POST['term']))
+                for tela in telas:
+                    item = tela.toJSON()
+                    item['value'] = tela.nombre
+                    data.append(item)
+            else:
+                data['error'] = 'No hay nada'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
